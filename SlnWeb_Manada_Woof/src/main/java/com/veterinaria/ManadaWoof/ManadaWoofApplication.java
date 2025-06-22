6d080c1932f980a5767056666c337b47264627b0
Jeison0729/ <farfan2@hotmail.es>
2025-06-21 23:23:51 -0500

    Start
    Agregando el proyecto inicial de solución Web

---
+++ SlnWeb_Manada_Woof/src/main/java/com/veterinaria/ManadaWoof/ManadaWoofApplication.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ManadaWoof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManadaWoofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManadaWoofApplication.class, args);
+	}
+
+}
---
+++ SlnWeb_Manada_Woof/src/test/java/com/veterinaria/ManadaWoof/ManadaWoofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinaria.ManadaWoof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManadaWoofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
