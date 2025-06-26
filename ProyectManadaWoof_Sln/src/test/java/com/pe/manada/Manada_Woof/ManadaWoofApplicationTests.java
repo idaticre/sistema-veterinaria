3893e8c1ecb8ad5a7d8a29885d5f58626fea2efb
Jeison0729/ <farfan2@hotmail.es>
2025-06-25 22:20:46 -0500

    Solucion web Spring Boot
    Se crea proyecto, revisar archivo pom.xml.
    Java 21
    Apache Netbeans 18

---
+++ ProyectManadaWoof_Sln/src/test/java/com/pe/manada/Manada_Woof/ManadaWoofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.manada.Manada_Woof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManadaWoofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
