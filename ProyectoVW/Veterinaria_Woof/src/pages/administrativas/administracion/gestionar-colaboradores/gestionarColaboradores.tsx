beaec37d2997914df45b0094e73a071ec5b906d6
Gabrielmirin <josegbenitesm@gmail.com>
2025-08-23 21:56:10 -0500

    Versión vieja

--- ProyectoVW/Veterinaria_Woof/src/pages/administrativas/administracion/gestionar-colaboradores/gestionarColaboradores.tsx
+++ ProyectoVW/Veterinaria_Woof/src/pages/administrativas/administracion/gestionar-colaboradores/gestionarColaboradores.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useRef, useState } from 'react'
+import Br_administrativa from '../../../../components/barra_administrativa/Br_administrativa'
+import { Link } from 'react-router-dom';
+// Importa los css de este CRUD luego
+
+interface Colaborador {
+    ID: number,
+    ID_ENTIDAD: number,
+    NOMBRE: string,
+    ID_USUARIO: number,
+    FOTO?: string
+}
+
 function gestionarColaboradores() {
     return (
         <>
-            <span>Texto de ejemplo!!!</span>
+            <div id="colaboradores">
+                <Br_administrativa onMinimizeChange={setMinimizado}/>
+                {/* yadda yadda ya */}
+            </div>
         </>
     )
 }
