2cd204588a05dbd3936d81d1be8d0116f57b6157
Gabrielmirin <josegbenitesm@gmail.com>
2025-10-13 14:10:53 -0500

    Updt

--- solucion-web-react/VW_pagina_administrativa/src/pages/administrativas/servicios/servicios.tsx
+++ solucion-web-react/VW_pagina_administrativa/src/pages/administrativas/servicios/servicios.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import Br_administrativa from '../../../components/barra_administrativa/Br_administrativa'
 import "./styles.css"
-import { Link } from "react-router-dom";
 
 interface Servicios {
     ID: number,
