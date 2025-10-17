f698deb9fc63285504745042a1ddbb48f9e11ef8
Gabrielmirin <josegbenitesm@gmail.com>
2025-10-16 22:17:26 -0500

    RAAAAHHH

--- Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vet.manadawoof.repository;
-
-import com.vet.manadawoof.entity.UsuarioEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
-}
