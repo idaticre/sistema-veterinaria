7d09f4e0627509462f26077659739192238d01f3
JoseG-BenitesM <josegbenitesm@gmail.com>
2025-10-16 21:57:04 -0500

    Delete Backend_VetManadaWoof_Api directory

--- Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vet.manadawoof.repository;
-
-import com.vet.manadawoof.entity.RolEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<RolEntity, Integer> {
-}
--- Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/repository/TipoDocumentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vet.manadawoof.repository;
-
-import com.vet.manadawoof.entity.TipoDocumentoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoDocumentoRepository extends JpaRepository<TipoDocumentoEntity, Integer> {
-}
