1667965df7253ef0b5ed274619accd00fac83fa9
Edgar-QuispeT <edgarquispet2305@hotmail.com>
2025-10-23 20:29:15 -0500

    para funcion de login

---
+++ Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/controller/loginController.java
@@ -0,0 +1,44 @@
+package com.vet.manadawoof.controller;
+
+import com.vet.manadawoof.service.LoginService;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+public class loginController {
+        
+    private final LoginService loginService ;
+    
+    @PostMapping
+    public ResponseEntity<?> login(@RequestBody Map<String, String> credenciales){
+       String username = credenciales.get("usuario");
+       String password = credenciales.get("password");
+       
+       var usuarioOpt = loginService.verificarUsuario(username, password);
+       
+       if(usuarioOpt.isPresent()){
+           return ResponseEntity.ok(Map.of(
+               "message", "Login exitoso",
+               "success", true
+           ));
+       }else{
+           return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of(
+               "message", "Usuario o contraseña incorrectos",
+               "success", false
+            ));
+       }
+    }
+    
+}
+
+//401 y HttpStatus.UNAUTHORIZED son lo mismo pero uno es mas tecnico
---
+++ Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/service/LoginService.java
@@ -0,0 +1,29 @@
+package com.vet.manadawoof.service;
+
+import com.vet.manadawoof.entity.UsuarioEntity;
+import com.vet.manadawoof.repository.UsuarioRepository;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+//import org.springdoc.security.cryto.bcrypt.BCryptPasswordEncoder;
+
+@Service
+public class LoginService {
+        
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    //private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    
+    public Optional <UsuarioEntity> verificarUsuario(String username, String password){
+        Optional<UsuarioEntity> usuarioOpt = usuarioRepository.findByUsername(username);
+        
+        if(usuarioOpt.isPresent()){
+            UsuarioEntity usuario = usuarioOpt.get();
+            if(usuario.getPasswordHash().equals(password)){
+                return Optional.of(usuario);
+            }
+        }
+        return Optional.empty();
+    }
+}
