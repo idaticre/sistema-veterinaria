fc24f7aaf5c473510b2b818a53d839492b321644
Edgar-QuispeT <edgarquispet2305@hotmail.com>
2026-03-29 22:57:17 -0500

    link de front

--- Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/config/SecurityConfig.java
+++ Backend_VetManadaWoof_Api/src/main/java/com/vet/manadawoof/config/SecurityConfig.java
@@ -140,7 +140,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://vet-woof-admin.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
